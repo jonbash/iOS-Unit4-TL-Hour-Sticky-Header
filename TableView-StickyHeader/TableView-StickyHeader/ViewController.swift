57027600ec304abecbc923cfc07155b80cfbb64e
Jon Bash <jonbash@protonmail.com>
2020-05-08 11:26:54 -0700

    added tableView & methods

--- TableView-StickyHeader/TableView-StickyHeader/ViewController.swift
+++ TableView-StickyHeader/TableView-StickyHeader/ViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 40
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)"
+
+        return cell
+    }
+
+    // MARK: - Table View Delegate
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        print("scrollView.y = \(scrollView.contentOffset.y)")
+    }
 }
 

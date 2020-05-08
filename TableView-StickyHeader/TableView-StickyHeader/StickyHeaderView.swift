603182e5b907989e30b77bdd7ebb1c100b8ca0d5
Jon Bash <jonbash@protonmail.com>
2020-05-08 11:56:12 -0700

    add headerView subviews & constraints

--- TableView-StickyHeader/TableView-StickyHeader/StickyHeaderView.swift
+++ TableView-StickyHeader/TableView-StickyHeader/StickyHeaderView.swift
@@ -10,7 +10,50 @@ import UIKit
 
 
 class StickyHeaderView: UIView {
-    // properties
+    var temperatureLabel: UILabel = {
+        let label = UILabel()
+        label.text = "98º"
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 60.0, weight: .light)
+        label.hasDropShadow = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    var cityLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Rochester, NY"
+        label.textColor = .white
+        label.hasDropShadow = true
+        label.font = UIFont.preferredFont(forTextStyle: .title1)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    var summaryLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Clear"
+        label.textColor = .white
+        label.hasDropShadow = true
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    var iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "partly-cloudy-day")
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        stackView.spacing = 0
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         setUpViews()
@@ -20,11 +63,45 @@ class StickyHeaderView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented; cannot use this view in storyboard")
     }
+
     func setUpViews() {
         backgroundColor = .systemBlue
+        stackView.addArrangedSubview(iconImageView)
+        stackView.addArrangedSubview(cityLabel)
+        stackView.addArrangedSubview(summaryLabel)
+        stackView.addArrangedSubview(temperatureLabel)
+        addSubview(stackView)
     }
 
     func setUpConstraints() {
+        iconImageView.addConstraints([
+            iconImageView.widthAnchor.constraint(equalToConstant: 60),
+            iconImageView.heightAnchor.constraint(equalToConstant: 60)
+        ])
+        let centerY = centerYAnchor.constraint(equalTo: stackView.centerYAnchor)
+        centerY.priority = .defaultHigh // can break when at top
+        let cityTopSpace = cityLabel.topAnchor.constraint(greaterThanOrEqualTo: self.layoutMarginsGuide.topAnchor, constant: 1)
+        cityTopSpace.priority = .defaultHigh + 1
+        addConstraints([
+            self.centerXAnchor.constraint(equalTo: stackView.centerXAnchor),
+            centerY,
+            cityTopSpace
+        ])
+    }
 
+    func updateViewForScrollPosition(y: CGFloat, width: CGFloat) {
+        var height = -y
+
+        let minHeight: CGFloat = 100
+        let defaultHeight: CGFloat = 300
+
+        if height < minHeight {
+            // prevent view from disappearing
+            height = minHeight
+        } else if height > defaultHeight {
+            // prevent view from growing
+            height = defaultHeight
+        }
+        frame = CGRect(origin: .zero, size: CGSize(width: width, height: height))
     }
 }
